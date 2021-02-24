d946db8397b064cae23d6b11716fad8d112b0f38
noorain7 <noorulain9457@outlook.in>
2021-02-24 19:42:39 +0530

    center_patch

--- config.def.h
+++ config.def.h
@@ -117,8 +117,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {1} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} }, 
 };
 
 /* button definitions */
