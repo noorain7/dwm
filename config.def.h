2f5badfb67475dc0dbf82121b90dc8bdf5d21e77
noorain7 <noorulain9457@outlook.in>
2021-02-24 20:14:51 +0530

    colorbar

--- config.def.h
+++ config.def.h
@@ -26,6 +26,11 @@ static char *colors[][4] = {
        /*               fg           bg           border	float   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor , "#282a36" },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor , "#282a36"  },
+	   [SchemeStatus]  = { "#000000", "#000000",  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	   [SchemeTagsSel]  = { selfgcolor, selbgcolor,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+       [SchemeTagsNorm]  = { normfgcolor, normbgcolor,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+       [SchemeInfoSel]  = { selbgcolor, selfgcolor,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+       [SchemeInfoNorm]  = { normfgcolor, normbgcolor,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
